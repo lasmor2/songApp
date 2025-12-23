54d159b0da6180aabd4405ae942879d70eb1222e
lasmor <llincohse@gmail.com>
2025-12-23 13:07:19 +0100

    implemente songs add, get, delete and update routes

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateSongDTO } from './dto/create-song-dto';
 import { SongsService } from './songs.service';
+import { Song } from './schemas/songs';
 
 @Controller('songs')
 export class SongsController {
@@ -11,5 +20,36 @@ export class SongsController {
     createSongDTO: CreateSongDTO,
   ) {
     return this.songService.create(createSongDTO);
+  }
+
+  @Get()
+  find(): Promise<Song[]> {
+    return this.songService.find();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id')
+    id: string,
+  ): Promise<Song | null> {
+    return this.songService.findById(id);
+  }
+
+  @Delete(':id')
+  delete(
+    @Param('id')
+    id: string,
+  ) {
+    return this.songService.delete(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id')
+    id: string,
+    @Body()
+    createSongDTO: CreateSongDTO,
+  ) {
+    return this.songService.update(id, createSongDTO);
   }
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -14,4 +14,19 @@ export class SongsService {
     const song = await this.songModel.create(createSongDTO); //3.
     return song;
   }
+  async find(): Promise<Song[]> {
+    return this.songModel.find();
+  }
+
+  async findById(id: string): Promise<Song | null> {
+    return this.songModel.findById(id);
+  }
+
+  async delete(id: string) {
+    return this.songModel.deleteOne({ _id: id });
+  }
+
+  async update(id: string, createSongDTO: CreateSongDTO) {
+    return this.songModel.updateOne({ _id: id }, createSongDTO);
+  }
 }
